48efd657e6f5e61cd7b43ea44a596b5d498a941f
Beth Schofield <bethmschofield@gmail.com>
2019-07-25 18:13:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,9 +19,9 @@ def speak_to_grandma(phrase)
   #unless phrase is hi nana i lowe you, in which case return
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
