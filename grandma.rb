842c4a2c89f0b1652c4f1553634c2b5ec9bd6bb9
Beth Schofield <bethmschofield@gmail.com>
2019-07-25 18:03:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(phrase)
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
+  elsif phrase == 
     "NO, NOT SINCE 1938!"
   else
     "I LOVE YOU TOO PUMPKIN!"
